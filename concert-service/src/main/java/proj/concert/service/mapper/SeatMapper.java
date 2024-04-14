25bdddcaffb3c357c2bf725747106d36b2a0d702
micattoc <micattoc@gmail.com>
2024-04-14 21:12:43 +1200

    Create and implement SeatMapper class

---
+++ concert-service/src/main/java/proj/concert/service/mapper/SeatMapper.java
@@ -0,0 +1,24 @@
+package proj.concert.service.mapper;
+
+import proj.concert.service.domain.Seat;
+
+/**
+ * Helper class to convert between domain-model and DTO objects representing Seats.
+ */
+public class SeatMapper {
+        static Seat toDomainModel(proj.concert.common.dto.SeatDTO dtoSeat) {
+            Seat fullSeat = new Seat(
+                    dtoSeat.getLabel(),
+                    dtoSeat.getPrice());
+            return fullSeat;
+        }
+
+        static proj.concert.common.dto.SeatDTO toDto(Seat seat) {
+            proj.concert.common.dto.SeatDTO dtoSeat =
+                    new proj.concert.common.dto.SeatDTO(
+                            seat.getLabel(),
+                            seat.getPrice());
+            return dtoSeat;
+        }
+    }
+}
