91dd55d147817a499382a1250105c157d4e638e9
micattoc <micattoc@gmail.com>
2024-04-19 21:13:11 +1200

    Add missing import statements and improve code readability

--- concert-service/src/main/java/proj/concert/service/mapper/UserMapper.java
+++ concert-service/src/main/java/proj/concert/service/mapper/UserMapper.java
@@ -8,18 +8,17 @@ import proj.concert.service.domain.User;
  */
 public class UserMapper {
 
-    static User toDomainModel(proj.concert.common.dto.UserDTO dtoUser) {
+    static User toDomainModel(UserDTO dtoUser) {
         User fullUser = new User(
-                dtoUser.getUsername(),
-                dtoUser.getPassword());
+                            dtoUser.getUsername(),
+                            dtoUser.getPassword());
         return fullUser;
     }
 
-    static proj.concert.common.dto.UserDTO toDto(User user) {
-        proj.concert.common.dto.UserDTO dtoUser =
-                new proj.concert.common.dto.UserDTO(
-                        user.getUsername(),
-                        user.getPassword());
+    static UserDTO toDto(User user) {
+        UserDTO dtoUser = new UserDTO(
+                            user.getUsername(),
+                            user.getPassword());
         return dtoUser;
     }
 }
