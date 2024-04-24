4298ab14de6b2f36ab79b5ef8edd33d25e182bf4
Isabel Body <isabelbody@gmail.com>
2024-04-24 13:43:42 +1200

    mapping each seat in Booking class

--- concert-service/src/main/java/proj/concert/service/mapper/BookingMapper.java
+++ concert-service/src/main/java/proj/concert/service/mapper/BookingMapper.java
@@ -1,7 +1,12 @@
 package proj.concert.service.mapper;
 
+import proj.concert.common.dto.SeatDTO;
 import proj.concert.service.domain.Booking;
 import proj.concert.common.dto.BookingDTO;
+import proj.concert.service.domain.Seat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Helper class to convert between domain-model and DTO objects representing Bookings.
@@ -9,10 +14,16 @@ import proj.concert.common.dto.BookingDTO;
 public class BookingMapper {
 
     public static BookingDTO toDto(Booking booking) {
+        List<SeatDTO> dtoSeats = new ArrayList<>();
+        for(Seat seat: booking.getSeats()){
+            SeatDTO dtoSeat = SeatMapper.toDto(seat);
+            dtoSeats.add(dtoSeat);
+        }
+
         BookingDTO dtoBooking = new BookingDTO(
                                     booking.getConcertId(),
                                     booking.getDate(),
-                                    booking.getSeats());
+                                    dtoSeats);
         return dtoBooking;
     }
 }
